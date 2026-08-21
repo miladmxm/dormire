42fc40d69d908b38b169dc4490de2885f384fad4
miladmxm <miladmxm12@gmail.com>
2026-08-21 16:51:44 +0330

    fix: fill and with , height

--- src/components/ui/defaultImage.tsx
+++ src/components/ui/defaultImage.tsx
@@ -8,6 +8,7 @@ const DefaultImage = ({
   alt,
   width,
   height,
+  fill,
   ...props
 }: Partial<Omit<ComponentProps<typeof Image>, "src">> & {
   image?: { url: string; alt?: string; id: string } | null;
@@ -20,15 +21,20 @@ const DefaultImage = ({
     );
   }
 
-  return (
-    <Image
-      src={image.url}
-      alt={alt || image.alt || "default image"}
-      width={width || 500}
-      height={width || 500}
-      {...props}
-    />
-  );
+  const imageProps: ComponentProps<typeof Image> = {
+    src: image.url,
+    alt: alt || image.alt || "default image",
+    ...props,
+  };
+
+  if (fill) {
+    imageProps.fill = fill;
+  } else {
+    imageProps.width = width || 500;
+    imageProps.height = width || 500;
+  }
+
+  return <Image {...imageProps} />;
 };
 
 export default DefaultImage;
